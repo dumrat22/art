47c20c3088e70c2708672be25b4c048045c362df
Duminda Rathnayaka <duminda_rathnayaka@proton.me>
2024-04-04 00:44:29 +0800

    General symmetric shape + animation of lotus

--- src/dumrat/art/petal.clj
+++ src/dumrat/art/petal.clj
@@ -8,7 +8,7 @@
 (m/use-primitive-operators)
 
 (defn mirror-curve-y
-  "Takes a curve, mirrors on y axis"
+  "Takes any curve, mirrors on y axis"
   [[c p]]
   [c
    (->> p
@@ -21,42 +21,31 @@
 
 (defn mirror-path-y
   [curves]
-  (into (mapv (fn [[c p]] [c (into [] (drop 2 p))]) curves)
-    (->> curves
-         (map mirror-curve-y)
-         (reverse)
-         (into []))))
+  (->> curves
+       (map mirror-curve-y)
+       (reverse)
+       (into [])
+       (into
+         (mapv #(update % 1 (fn [v] (into [] (drop 2 v)))) curves))))
 
 (comment
 
   (partition 2 [-100 200 0 180 0 200])
 
   (mirror-curve-y [:cubic [0 0 -100 200 0 180 0 200]])
-  (mirror-path-y [[:cubic [0 0 -100 200 0 180 0 200]]])
+  (mirror-path-y [[:move [0 0]] [:cubic [-100 200 0 180 0 200]]])
+
+  (mapv #(update % 1 (fn [v] (into [] (drop 2 v))))
+     [[:move [0 0 0 0]]
+      [:cubic [0 0 -100 200 0 180 0 200]] false])
 
   #_f)
 
-(defn symmetric-petal-shape
-  "Create petal shape given a run of cubics that describe one half of the petal at r radius from given center"
-  []
+(defn symmetric-shape
+  "Create shape given a run of moves that describe one half of the petal"
+  [path]
   (c2d/path-def->shape
-    (into
-     (into
-      [[:move [0 0]]]
-      (mirror-path-y
-       [[:cubic [0 0 -100 100 -80 120 -50 120]]
-        [:cubic [-50 120 -30 150 -10 165 0 200]]]))
-     [[:move [0 0]]])))
-
-(defn petal-shape []
-  (c2d/path-def->shape
-      [[:move [-50 87]]
-       [:cubic [-100 200 0 180 0 200]]
-       [:cubic [0 180 100 200 50 87]]
-       [:cubic [25 100 -25 100 -50 87]]]))
-
-(defn draw-petal [canvas]
-  (c2d/shape canvas (symmetric-petal-shape)))
+    (mirror-path-y path)))
 
 (defonce esc-pressed? (atom false))
 
@@ -72,18 +61,39 @@
           elapsed-time (- curr-time start-time)
           frame-rate (/ (* frameno 1000000000.0) elapsed-time)]
       (-> canvas
+          (c2d/scale 2)
           (c2d/set-color 255 255 255)
           (c2d/rect 0 0 (c2d/width canvas) (c2d/height canvas))
           (c2d/set-color 0 0 0)
-          (c2d/text (format "%2f" frame-rate) 20 20 :left)
-          (c2d/translate 400 400)
+          (c2d/set-stroke 1)
+          (c2d/text (format "%f" frame-rate) 20 20 :left)
+          (c2d/translate 200 200)
+          (c2d/rotate (/ frameno m/TWO_PI 240))
           (c2d/flip-y)
-          #_(draw-petal))
-      (c2d/rotate canvas (/ frameno m/TWO_PI 60))
-      #_(c2d/pattern-mode canvas (:texture state) -100 -100 400 400)
+          (c2d/set-color 0 0 0)
+          (c2d/ellipse 0 0 250 250)
+          (c2d/set-color 255 255 255)
+          (c2d/ellipse 0 0 248 248)
+          (c2d/set-color 0 0 0)
+          (c2d/ellipse 0 0 242 242)
+          (c2d/set-color 232 177 40)
+          (c2d/ellipse 0 0 242 242))
       (dotimes [i 6]
-        (draw-petal canvas)
+        (c2d/set-color canvas (+ 120 (* (m/sin (/ frameno m/TWO_PI 60)) 40)) 0 0)
+        (c2d/shape canvas
+                   (symmetric-shape
+                    [[:move [0 0 0 0]]
+                     [:line [0 0 -50 87]]
+                     [:cubic [-50 87 -60 120 0 90 0 120]]]))
         (c2d/rotate canvas (/ m/TWO_PI 6)))
+      ;; rect
+      (-> canvas
+        (c2d/set-color 0 0 0)
+        (c2d/rect -50 -50 100 100)
+        (c2d/rotate m/QUARTER_PI)
+        (c2d/rect -50 -50 100 100)
+        (c2d/set-color 255 255 255)
+        (c2d/ellipse 0 0 100 100))
       state)))
 
 (def window (c2d/show-window {:canvas (c2d/canvas 800 800 :mid)
@@ -91,7 +101,7 @@
                               :hint :mid
                               :always-on-top? true
                               :position [0 0]
-                              :draw-fn draw
+                              :draw-fn #'draw
                               :setup (fn [c _]
                                        (c2d/set-background c 45 45 41)
                                        {:start-time (. System (nanoTime))
